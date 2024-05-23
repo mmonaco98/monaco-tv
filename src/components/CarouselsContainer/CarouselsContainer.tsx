feeac7d9d1e9911b541574ff30bfb45f3d3d4620
Marco Monaco <m.monaco@reply.it>
2024-05-23 22:32:50 +0200

    Created HomePage components

---
+++ src/components/CarouselsContainer/CarouselsContainer.tsx
@@ -0,0 +1,28 @@
+import { FlatList, View, StyleSheet } from 'react-native';
+import { Carousel } from '../Carousel/Carousel';
+import { HomeData } from '../../helpers/fakeData';
+import { AppColors } from '../../enums/colors';
+
+const style = StyleSheet.create({
+    container: {
+        height: '40%',
+        width: '100%',
+    },
+});
+
+export const CarouselsContainer = (): JSX.Element => {
+    return (
+        <>
+            <View style={style.container}>
+                <FlatList
+                    showsHorizontalScrollIndicator={false}
+                    showsVerticalScrollIndicator={false}
+                    data={HomeData.sections}
+                    renderItem={({ item, index }) => {
+                        return <Carousel data={item.data} type={item.type} />;
+                    }}
+                ></FlatList>
+            </View>
+        </>
+    );
+};
---
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { useStore } from '../../stores/appStores';
+import { hScale, vScale } from '../../helpers/sizeHelper';
+import { AppColors } from '../../enums/colors';
+
+const style = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '60%',
+        flexDirection: 'row',
+    },
+    imageContainer: {
+        height: '100%',
+        width: '65%',
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        zIndex: -1,
+    },
+    textContainer: {
+        height: '100%',
+        width: '50%',
+    },
+    title: {
+        color: AppColors.white,
+        fontSize: vScale(80),
+    },
+    releaseYear: {},
+    description: {},
+});
+
+export const MoviePreview = (): JSX.Element => {
+    const focusedItem = useStore((state) => state.focusedItem);
+
+    return (
+        <>
+            <View style={style.container}>
+                <View style={style.textContainer}>
+                    <Text style={style.title}>{focusedItem.movie_title}</Text>
+                    <Text style={style.releaseYear}>
+                        {focusedItem.movie_release_year}
+                    </Text>
+                    <Text style={style.description}>
+                        {focusedItem.movie_description}
+                    </Text>
+                </View>
+                <View style={style.imageContainer}>
+                    <Image
+                        src={focusedItem.movie_image_url}
+                        style={{ width: '100%', height: '100%' }}
+                        resizeMode="cover"
+                    />
+                </View>
+            </View>
+        </>
+    );
+};
