b11762601633dacab22aa03a5de06837f3e79c80
Marco Monaco <m.monaco@reply.it>
2024-05-23 22:33:06 +0200

    Fixed HomePage with new components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,17 @@ import { FlatList, Pressable, StyleSheet, View } from 'react-native';
 import { AppColors } from '../enums/colors';
 import { HomeData } from '../helpers/fakeData';
 import { Carousel } from '../components/Carousel/Carousel';
+import { API_URL } from '../../ENV';
+import { useEffect, useState } from 'react';
+import { TMovie } from '../types/movie';
+import { useStore } from '../stores/appStores';
+import { CarouselsContainer } from '../components/CarouselsContainer/CarouselsContainer';
+import { MoviePreview } from '../components/MoviePreview/MoviePreview';
 
 const style = StyleSheet.create({
-    container: {
+    homePage: {
         height: '100%',
         width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
         backgroundColor: AppColors.background,
     },
 });
@@ -17,15 +20,9 @@ const style = StyleSheet.create({
 export const HomePage = (): JSX.Element => {
     return (
         <>
-            <View style={style.container}>
-                <FlatList
-                    showsHorizontalScrollIndicator={false}
-                    showsVerticalScrollIndicator={false}
-                    data={HomeData.sections}
-                    renderItem={({ item, index }) => {
-                        return <Carousel data={item.data} type={item.type} />;
-                    }}
-                ></FlatList>
+            <View style={style.homePage}>
+                <MoviePreview />
+                <CarouselsContainer />
             </View>
         </>
     );
