cd0743239f8cca6b494c60ceba8e06c43d86abfa
Marco Monaco <m.monaco@reply.it>
2024-05-27 11:07:06 +0200

    Add type and api call

--- src/types/user.ts
+++ src/types/user.ts
@@ -7,4 +7,5 @@ export interface TUser {
     gender: TGender;
     age: number;
     avatar: string;
+    password: string;
 }
--- src/utils/rest-api.ts
+++ src/utils/rest-api.ts
@@ -1,6 +1,7 @@
 import { API_URL } from '../../ENV';
 import { TMovie } from '../types/movie';
 import { TSection } from '../types/section';
+import { TUser } from '../types/user';
 
 export const getMovieById = async (id: number): Promise<TMovie> => {
     return await fetch('http://' + API_URL + `/getMovieData/byId?id=${id}`, {
@@ -33,4 +34,21 @@ export const getHomepageByUserId = async (id: number): Promise<TSection[]> => {
             return json.data;
         })
         .catch(() => {});
+};
+
+export const getUserById = async (id: number): Promise<TUser | undefined> => {
+    return await fetch('http://' + API_URL + `/users/byId?id=${id}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            return json.data;
+        })
+        .catch((err) => {
+            return undefined;
+        });
 };
